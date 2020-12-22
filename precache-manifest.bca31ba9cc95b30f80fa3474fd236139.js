5d9ca75dd8ab1e0e9e0cf3f273ea32c31e335684
nautilytics <chris@nautilytics.com>
2020-12-22 16:30:06 +0000

    Deploying to gh-pages from  @ 0d018a5b31b1eb7cbb2619485ebe1cbd51c32f39 🚀

---
+++ precache-manifest.bca31ba9cc95b30f80fa3474fd236139.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57e163aa88b94d714470e578701ce2a",
+    "url": "/unhcr-refugee-project/index.html"
+  },
+  {
+    "revision": "0b5c00ca405cbb2b53ae",
+    "url": "/unhcr-refugee-project/static/css/2.0bb522a0.chunk.css"
+  },
+  {
+    "revision": "4ac673e6b1f087a5bb16",
+    "url": "/unhcr-refugee-project/static/css/main.26a38083.chunk.css"
+  },
+  {
+    "revision": "0b5c00ca405cbb2b53ae",
+    "url": "/unhcr-refugee-project/static/js/2.c4570168.chunk.js"
+  },
+  {
+    "revision": "e7f143aaefb4a3c71ed95c1c7c835540",
+    "url": "/unhcr-refugee-project/static/js/2.c4570168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac673e6b1f087a5bb16",
+    "url": "/unhcr-refugee-project/static/js/main.b8bfc2b5.chunk.js"
+  },
+  {
+    "revision": "8a4746ea9ea22e10a333",
+    "url": "/unhcr-refugee-project/static/js/runtime-main.3b4935ce.js"
+  },
+  {
+    "revision": "7200c1f86ea867da68ab2cc374a2a96d",
+    "url": "/unhcr-refugee-project/static/media/filter.7200c1f8.svg"
+  },
+  {
+    "revision": "ccb81835522cb137e780646f7e4523e7",
+    "url": "/unhcr-refugee-project/static/media/refugees.ccb81835.csv"
+  },
+  {
+    "revision": "67e4e3d0205d848d40de9bbc6383d121",
+    "url": "/unhcr-refugee-project/static/media/search.67e4e3d0.svg"
+  }
+]);
